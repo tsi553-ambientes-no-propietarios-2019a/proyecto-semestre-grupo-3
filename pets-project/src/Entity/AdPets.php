bebc35d271813cf5a0af03fe72d0896b569723d8
Dayana Rodriguez <dayana.rodriguez@epn.edu.ec>
2019-07-23 20:45:19 -0500

    Imagenes

--- pets-project/src/Entity/AdPets.php
+++ pets-project/src/Entity/AdPets.php
@@ -53,7 +53,7 @@ class AdPets
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      * 
-     * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName", size="imageSize")
+     * @Vich\UploadableField(mapping="ad_image", fileNameProperty="imageName", size="imageSize")
      * 
      * @var File
      */
